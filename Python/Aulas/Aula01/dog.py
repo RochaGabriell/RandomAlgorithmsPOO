d34a2c167f17ad88df3039afa4ab69ad621c3713
RochaGabriell <gabrielrocha1902@gmail.com>
2022-08-18 11:45:11 -0300

    Python Aula01

---
+++ Python/Aulas/Aula01/dog.py
@@ -0,0 +1,18 @@
+class Dog():
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    
+    def sentar(self):
+        print(self.nome.title() + " sentar agora")
+
+    def rolar(self):
+        print(self.nome.title() + " rolar")
+    
+meu_cachorro = Dog("Osca", 3)
+
+# Acessar uma determinada váriavel
+print(f"Meu cachorro se chama {meu_cachorro.nome}, e tem {meu_cachorro.idade} anos.")
+
+# Acessar um metodo
+meu_cachorro.sentar()
